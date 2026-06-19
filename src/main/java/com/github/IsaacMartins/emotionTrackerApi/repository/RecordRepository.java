bf0ed670bafd5e585cd8cf4b5eb693414bf28e4d
Isaac Martins <isaacmartins.code@gmail.com>
2026-06-19 11:51:29 -0300

    fix: correção no tipo definido na herança de JpaRepository de Record java.lang para Record entidade

--- src/main/java/com/github/IsaacMartins/emotionTrackerApi/repository/RecordRepository.java
+++ src/main/java/com/github/IsaacMartins/emotionTrackerApi/repository/RecordRepository.java
@@ -1,8 +1,9 @@
 package com.github.IsaacMartins.emotionTrackerApi.repository;
 
+import com.github.IsaacMartins.emotionTrackerApi.entities.record.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
-public interface RecordRepository extends JpaRepository<Record , UUID> {
+public interface RecordRepository extends JpaRepository<Record, UUID> {
 
 }
